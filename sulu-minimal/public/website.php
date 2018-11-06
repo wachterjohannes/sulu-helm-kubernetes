85cde00e9c4a48a93a120f564f18699d0b8f0787
Johannes Wachter <johannes@sulu.io>
2018-11-06 19:19:17 +0100

    configured sulu to use varnish and use fork with bugfix

--- sulu-minimal/public/website.php
+++ sulu-minimal/public/website.php
@@ -58,13 +58,13 @@ $kernel = new Kernel($env, $debug, SuluKernel::CONTEXT_WEBSITE);
 
 // Comment this line if you want to use the "varnish" http
 // caching strategy. See http://sulu.readthedocs.org/en/latest/cookbook/caching-with-varnish.html
-if ($env !== 'dev') {
-    $kernel = new SuluHttpCache($kernel);
+//if ($env !== 'dev') {
+//    $kernel = new SuluHttpCache($kernel);
 
     // When using the HttpCache, you need to call the method in your front controller
     // instead of relying on the configuration parameter
-    Request::enableHttpMethodParameterOverride();
-}
+//    Request::enableHttpMethodParameterOverride();
+//}
 
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
